0607703f49dfd9b56542a7cbcbb6b67776d13517
Philipp Jost <philx_x@hotmail.com>
2016-06-21 00:16:23 -0400

    refactor toggle-todos

--- dialog.rkt
+++ dialog.rkt
@@ -5,25 +5,45 @@
 (require "system.rkt")
 (provide toggle-todos add-todo)
 
-(define toggle-todos (lambda (todo-list)
-  ; adjust list length dynamically to the amount of todo items
-  (define args (todo-list->shell-options todo-list))
-  (define options(list* "--cancel-button" "gtfo" "--title" "YATλ!" "--checklist" "Todos" "18" "60" (number->string (length todo-list)) args))
-  (map string->number
-       (string-split ; "1 3 4" -> '("1" "3" "4")
-        (call-with-output-string
-         (lambda (listener-port)
-           ((fifth ; grab the fifth return value from the system call
-             ; spawn whiptail/dialog process and redirect the stderr to the call-with-string (p)
-             (apply process*/ports (current-output-port) (current-input-port) listener-port (find-executable-path "dialog") options))'wait)))))))
+; takes a list of (tag item status)s
+; returns the new state of the todo list after toggling
+(define toggle-todos
+  (lambda (todo-list)
+    (let* ([menu-heigth (number->string (length todo-list))]
+           [window-options (list "--title" "YATΛ!" "--checklist" "Toggle Todos" WINDOW-HEIGHT WINDOW-WIDTH menu-heigth)]
+           [todo-list-options (todo-list->shell-options todo-list)]
+           [arguments (append window-options todo-list-options)])
+
+      (map cdr ; removes the index again
+           ; TODO: refactor
+           (apply-status ; returns todo-list with completed field true or false based on the complted indices list
+            (zip-with-index (sort todo-list #:key caddr <) 1)
+            (map string->number
+                 (string-split ; "1 3 4" -> '("1" "3" "4")
+                  (dialog->string arguments)))))))) ; returns the indices of the checked todos
+
+
 
 
 (define add-todo (lambda ()
-                  (let* ([todo-text-options (list "--clear" "--inputbox" "New Todo: Content" WINDOW-HEIGHT WINDOW-WIDTH)]
-                         [todo-priority-options (list "--clear" "--menu" "New Todo: Priority" WINDOW-HEIGHT WINDOW-WIDTH "3" "1" "High" "2" "Medium" "3" "Low")])
-                    (define text (dialog->string todo-text-options))
-                    (define priority (string->number(dialog->string todo-priority-options)))
+                  (let ([add-todo-text-options (list "--clear" "--title" "YATΛ!" "--inputbox" "New Todo: Content" WINDOW-HEIGHT WINDOW-WIDTH)]
+                        [add-todo-priority-options (list "--clear" "--title" "YATΛ!" "--menu" "New Todo: Priority" WINDOW-HEIGHT WINDOW-WIDTH "3" "1" "High" "2" "Medium" "3" "Low")])
+                    (define text (dialog->string add-todo-text-options))
+                    (define priority (string->number(dialog->string add-todo-priority-options)))
                     (list text #f priority))))
+
+; returns todo-list with completed field true or false based on the complted indices list
+(define apply-status
+(lambda (todo-list completed-indices)
+  ; map over todo itmes
+  (map (lambda (todo-item)
+         ; if the todo item's index is member of the completed indicies
+         (cond [(member (string->number(car todo-item)) completed-indices)
+                ; set it's completed status to true
+                (list-set todo-item 2 #t)]
+               [else
+                (list-set todo-item 2 #f)]))
+       todo-list)))
 
 ; to todo-list->shell-options -> sanitize to use as argument. ex: #f to OFF
 (define argify-status (lambda (todo) (if (cadr todo)
--- yata.rkt
+++ yata.rkt
@@ -6,37 +6,9 @@
 
 (define todos (load-list))
 
-; (zip-with-index '("@" "!" "%") 3) -> '(3 . "@") (4 . "!") (5 . "%"))
-(define zip-with-index (lambda (lst start-index)
-                         ; define a generators of natural integers starting from start-index
-                         (letrec ([naturals (sequence->generator (in-naturals start-index))]
-                                  [zip-with-naturals (lambda (lst)
-                                                       (cond ((empty? lst) empty)
-                                                             (else (cons
-                                                                    ; cons (1 "a")
-                                                                    (cons
-                                                                          (number->string(naturals))
-                                                                          (car lst))
-                                                                    ; with the recursive call on the lst's tail
-                                                                    (zip-with-naturals(cdr lst))))))])
-                           (zip-with-naturals lst))))
-
-; returns todo-list with completed field true or false based on the complted indices list
-(define apply-status
-  (lambda (todo-list completed-indices)
-    ; map over todo itmes
-    (map (lambda (todo-item)
-           ; if the todo item's index is member of the completed indicies
-           (cond [(member (string->number(car todo-item)) completed-indices)
-                  ; set it's completed status to true
-                  (list-set todo-item 2 #t)]
-                 [else
-                  (list-set todo-item 2 #f)]))
-         todo-list)))
-
-
-(write (cons (add-todo) todos))
 ; toggling todos and save result
-(save-list (map cdr (apply-status (zip-with-index (sort todos #:key caddr <) 1) (toggle-todos todos))))
+;(write (cons (add-todo) todos))
+
+(save-list (toggle-todos todos))
 
 (system "clear")
