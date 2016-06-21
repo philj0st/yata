b6b45f13fe89f01318bde9b5d2890b26a6ce5af8
Philipp Jost <philx_x@hotmail.com>
2016-06-21 17:50:05 -0400

    improve options

--- yata.rkt
+++ yata.rkt
@@ -4,25 +4,30 @@
 (require "dialog.rkt")
 
 (define add-todo-flag? (make-parameter #f))
-(define menu-flag? (make-parameter #f))
-(define help-flag? (make-parameter #f))
+(define toggle-flag? (make-parameter #f))
 
 (define mode
   (command-line
    #:program "yata"
    #:once-each
    [("-n" "--new") "Add new Todo"
-                       (save-list (cons (add-todo) (load-list)))]
+                       (add-todo-flag? #t)]
    [("-m" "--menu") "Display graphical menu"
-                       (menu-flag? #t)]
+                       (toggle-flag? #t)]
    ))
 
+;(write (add-todo))
 
-;(define todos (load-list))
+(when (add-todo-flag?)
+ (let ([new-todo (add-todo)])
+   (cond [(list? new-todo) (save-list (cons new-todo (load-list)))]
+         [else (write "Add Todo was cancelled")])))
 
-; toggling todos and save result
-(save-list (toggle-todos (load-list)))
-;(save-list (cons (add-todo) (load-list)))
-
+(when (toggle-flag?)
+ ; capture the result of the toggle-dialog
+ (let ([toggled-todos (toggle-todo (load-list))])
+   ; only save them if the dialog wasn't cancelled
+   (cond [(list? toggled-todos) (save-list (cons new-todo (load-list)))]
+         [else (write "Add Todo was cancelled")])))
 
 (system "clear")
