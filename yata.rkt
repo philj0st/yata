0ac6008b0a659c6d80e2f4ecaa1546aabee934e4
Philipp Jost <philx_x@hotmail.com>
2016-06-21 20:29:19 -0400

    add remove-completed feature

--- yata.rkt
+++ yata.rkt
@@ -3,7 +3,15 @@
 (require "filesystem.rkt")
 (require "dialog.rkt")
 
+(define remove-completed
+ (lambda (todo-list)
+   (filter (lambda (todo-item)
+             ; filters for the todo items that have a #f at the cadr spot
+             (not (cadr todo-item)))
+           todo-list)))
+
 (define add-todo-flag? (make-parameter #f))
+(define remove-completed-flag? (make-parameter #f))
 (define toggle-flag? (make-parameter #f))
 
 (define mode
@@ -12,14 +20,24 @@
    #:once-each
    [("-n" "--new") "Displays a dialog to Add a new Todo"
                        (add-todo-flag? #t)]
+   [("-r" "--remove-completed") "Removes all completed Todos"
+                       (remove-completed-flag? #t)]
    [("-t" "--toggle") "Displays a dialog to toggle Todos"
                        (toggle-flag? #t)]
    ))
+
+
+; if there are no flags set run default action
+(when (zero? (vector-length (current-command-line-arguments)))
+  (toggle-flag? #t))
 
 (when (add-todo-flag?)
  (let ([new-todo (add-todo)])
    (cond [(list? new-todo) (save-list (cons new-todo (load-list)))]
          [else (write "Add Todo was cancelled")])))
+
+(when (remove-completed-flag?)
+ (save-list (remove-completed (load-list))))
 
 (when (toggle-flag?)
  ; capture the result of the toggle-dialog
@@ -27,5 +45,6 @@
    ; only save them if the dialog wasn't cancelled
    (cond [(list? new-todos-state) (save-list new-todos-state)]
          [else (write "Toggling Todos was cancelled")])))
+
 
 (system "clear")
